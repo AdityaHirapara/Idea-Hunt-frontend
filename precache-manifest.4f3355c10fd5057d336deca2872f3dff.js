ba7365d8e4b0e14d346f083f8b9c9c65a9ccfda0
AdityaHirapara <adityahirapara2016@gmail.com>
2019-04-22 19:07:39 +0530

    Updates

---
+++ precache-manifest.4f3355c10fd5057d336deca2872f3dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "033aa1693865ef40796393356214de73",
+    "url": "/idea-hunt-frontend/static/media/bg2.033aa169.jpg"
+  },
+  {
+    "revision": "1b80cfb4cbd4c1bfb94c",
+    "url": "/idea-hunt-frontend/static/js/runtime~main.783c5050.js"
+  },
+  {
+    "revision": "be3d5fdf9eaf4454292d",
+    "url": "/idea-hunt-frontend/static/js/main.575b2ca9.chunk.js"
+  },
+  {
+    "revision": "d3c74a7de47c1dec1011",
+    "url": "/idea-hunt-frontend/static/js/2.9573cec2.chunk.js"
+  },
+  {
+    "revision": "be3d5fdf9eaf4454292d",
+    "url": "/idea-hunt-frontend/static/css/main.3f6d1d35.chunk.css"
+  },
+  {
+    "revision": "2e36177dc2783f99842a2cae8f6a2f27",
+    "url": "/idea-hunt-frontend/index.html"
+  }
+];
